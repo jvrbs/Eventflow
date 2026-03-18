dd9af4e0c8ba182bc3bdee50b2d86d0351387040
Joao Ribas <jv.simao10@gmail.com>
2026-03-18 20:50:49 -0300

    estrutura inicial do projeto eventflow

---
+++ backend/eventflow/src/test/java/br/pucpr/eventflow/EventflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.eventflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
