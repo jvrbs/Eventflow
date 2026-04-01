3dffd921789aa9ca4fa332427b6bd46aafe02c0a
Joao Ribas <jv.simao10@gmail.com>
2026-04-01 19:21:00 -0300

    Removi o python Flask do projeto

--- backend/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-def create_app():
-    #define app como objeto Flask
-    app = Flask(__name__)
-    return app
--- backend/app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'ProjetoAvangers123'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:PUC%401234@localhost/eventflow'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
