80d809305ab13c81ecfdba5b9b872f1849b22ff1
Joao Ribas <jv.simao10@gmail.com>
2026-04-01 21:33:38 -0300

    feat: validação JS nos formulários de login e cadastro

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,23 +1,46 @@
-const formulario = document.getElementById('formCadastro');
+const formularioCadastro = document.getElementById('formCadastro');
+const formularioLogin = document.getElementById('formLogin')
+const mensagem = document.getElementById('mensagem');
+const mensagem2 = document.getElementById('mensagem2')
+const mensagem3 = document.getElementById('mensagem3')
 
-formulario.addEventListener('submit', function(event) {
+
+if (formularioCadastro) {
+    formularioCadastro.addEventListener('submit', function(event) {
     // 1. ISSO É O MAIS IMPORTANTE:
     // Impede o formulário de tentar ir para o "action", evitando o erro 405
     event.preventDefault(); 
+        const senha = document.getElementById('passwordCadastro').value;
+        const confirma = document.getElementById('confirmarSenhaCadastro').value;
 
-    const senha = document.getElementById('senhaCadastro').value;
-    const confirma = document.getElementById('confirmarSenhaCadastro').value;
-
-    if (senha !== confirma) {
-        alert("As senhas não coincidem!");
-    } else {
-        alert("Cadastro realizado com sucesso!");
-        
-        // 2. O Redirecionamento Manual:
-        // Como você está na pasta /cadastro/, se o login estiver uma pasta atrás:
-        window.location.href = "../login/login.html"; 
-        
-        // Se estiverem na mesma pasta, use apenas:
-        // window.location.href = "login.html";
+        if (senha !== confirma) {
+            mensagem.style.display = 'block';
+        } else if (senha.length < 8){
+            mensagem2.style.display = 'block'           
+        } else {
+            // 2. O Redirecionamento Manual:
+            // Como você está na pasta /cadastro/, se o login estiver uma pasta atrás:
+            window.location.href = "../login/login.html"; 
+            
+            // Se estiverem na mesma pasta, use apenas:
+            // window.location.href = "login.html";
+        }
     }
-});
+);}
+if (formularioLogin){
+formularioLogin.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+        const email = document.getElementById('emailLogin').value;
+        const senha = document.getElementById('passwordLogin').value;
+
+
+    if (email == '' || senha == ''){
+        mensagem3.style.display = 'block';
+    }
+
+        
+    
+
+
+});}
